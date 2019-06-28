9377f65c3f1d0e7a65c1bc9a9392647cdd47a140
Vinayak Grover <grover.vinayak0611@gmail.com>
2019-06-28 16:15:14 +0530

    Done adding map function

--- Src/App/Components/Calculator.js
+++ Src/App/Components/Calculator.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import "../../Stylesheets/CalculatorStyle.css";
-export class Calculator extends React.Component{
+export class Calculator extends React.Component{	
 	render(){
+		const numbers=["1","2","3","+","4","5","6","-","7","8","9","X","C","0","=","/"];
 		return(
 			<div>
 				<div className="top-page">
@@ -13,54 +14,11 @@ export class Calculator extends React.Component{
 						</div>
 						<div className="button-whole">
 							<div className="button-in">
-								<div className="single-button">
-									<p>1</p>
-								</div>
-								<div className="single-button">
-									<p>2</p>
-								</div>
-								<div className="single-button">
-									<p>3</p>
-								</div>
-								<div className="single-button">
-									<p>+</p>
-								</div>
-								<div className="single-button">
-									<p>4</p>
-								</div>
-								<div className="single-button">
-									<p>5</p>
-								</div>
-								<div className="single-button">
-									<p>6</p>
-								</div>
-								<div className="single-button">
-									<p>-</p>
-								</div>
-								<div className="single-button">
-									<p>7</p>
-								</div>
-								<div className="single-button">
-									<p>8</p>
-								</div>
-								<div className="single-button">
-									<p>9</p>
-								</div>
-								<div className="single-button">
-									<p>X</p>
-								</div>
-								<div className="single-button">
-									<p>C</p>
-								</div>
-								<div className="single-button">
-									<p>0</p>
-								</div>
-								<div className="single-button">
-									<p>=</p>
-								</div>
-								<div className="single-button">
-									<p>/</p>
-								</div>
+								{numbers.map(number=>
+									<div className="single-button">
+										<p>{number}</p>
+									</div>
+								)}
 							</div>
 						</div>
 					</div>
